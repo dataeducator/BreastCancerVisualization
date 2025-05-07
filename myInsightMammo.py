a8de313dd4d1a37cfc126d71c709d8c6a4836c10
Tenicka Norwood <107881738+dataeducator@users.noreply.github.com>
2025-05-07 11:59:33 -0500

    Update myInsightMammo.py

--- myInsightMammo.py
+++ myInsightMammo.py
@@ -5,7 +5,7 @@ import sys
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", "joblib"])
 
-import joblib
+import pickle
 import numpy as np
 import pandas as pd
 import plotly.express as px
